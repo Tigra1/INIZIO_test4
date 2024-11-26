4bab8ce7ecb52715e50551d15a2ca8ebd71a03f7
Tigra1 <idontsov@seznam.cz>
2024-11-26 19:33:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, render_template, send_file
 import requests
 from bs4 import BeautifulSoup
 import os
+import logging
 
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.DEBUG)
 # Директория для сохранения файлов
 SAVE_DIR = "results"
 os.makedirs(SAVE_DIR, exist_ok=True)
@@ -25,7 +26,7 @@ def search_google_and_save():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     }
     search_url = f"https://www.google.com/search?q={query}&hl=en"
-
+    logging.debug(f"URL: {search_url}")  # Логируем URL
     response = requests.get(search_url, headers=headers)
     if response.status_code != 200:
         return "Ошибка при выполнении запроса!", 500
@@ -48,6 +49,10 @@ def search_google_and_save():
 
     # Отправляем файл пользователю для скачивания
     return send_file(filename, as_attachment=True)
+
+except Exception as e:
+        logging.error(f"Ошибка при обработке запроса: {e}")
+        return "Внутренняя ошибка сервера.", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
